7382100e1189cd27f85d250208b4a1fd48fa6e29
rainbowgt <hysun@sei.ecnu.edu.cn>
2018-12-12 14:02:25 +0800

    Initial Project

---
+++ JenkinsGradleDemo/src/main/java/edu/ecnu/sei/st/JenkinsGradleDemoClass.java
@@ -0,0 +1,24 @@
+package edu.ecnu.sei.st;
+
+public class JenkinsGradleDemoClass {
+	
+	 public int absSum(Integer op1, Integer op2) {
+			
+			if((op1 == null) && (op2 == null)){
+				return 0;
+			}
+			
+			
+			if((op1 == null) &&( op2 != null)){
+				return Math.abs(op2);
+			}
+			
+
+			if(op2 == null) {
+				return Math.abs(op1);
+			}
+			
+			return Math.abs(op1)+Math.abs(op2);
+		}
+
+}
---
+++ JenkinsGradleDemo/src/test/java/edu/ecnu/sei/st/JenkinsGradleDemoClassTest.java
@@ -0,0 +1,60 @@
+package edu.ecnu.sei.st;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JenkinsGradleDemoClassTest {
+
+	private JenkinsGradleDemoClass demo;
+	
+	@Before
+	public void init() {
+		demo = new JenkinsGradleDemoClass();
+	}
+	
+
+	@Test
+	public void op1_is_null_and_op2_is_null() {
+		 
+		Integer op1 = null;
+		Integer op2 = null;
+		int ExpectedRlt = 0;		
+		int actualRlt = demo.absSum(op1, op2);		
+		assertEquals(ExpectedRlt,actualRlt);
+	}
+	
+
+	@Test
+	public void op1_is_null_but_op2_isnot_null() {
+		Integer op1 = null;
+		Integer op2 = new Integer("5");
+		int ExpectedRlt = 5;		
+		int actualRlt = demo.absSum(op1, op2);		
+		assertEquals(ExpectedRlt,actualRlt);
+	}
+	
+
+	@Test
+	public void op1_isnot_null_and_op2_is_null() {
+		
+		Integer op1 = new Integer("10");
+		Integer op2 = null ;
+		int ExpectedRlt = 10;		
+		int actualRlt = demo.absSum(op1, op2);		
+		assertEquals(ExpectedRlt,actualRlt);
+		
+	}
+		
+	@Test
+	public void op1_isnot_null_and_op2_isnot_null() {
+		
+		Integer op1 = new Integer("10");
+		Integer op2 = new Integer("5");
+		int ExpectedRlt = 15;		
+		int actualRlt = demo.absSum(op1, op2);		
+		assertEquals(ExpectedRlt,actualRlt);		
+	}
+
+}
